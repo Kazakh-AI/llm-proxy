ab1334a45caf9a1d6a65e69fdce6a249c670b776
Kirill Saidov <kirillsaidovd@yahoo.com>
2025-04-13 10:28:23 +0500

    update

--- src/main.py
+++ src/main.py
@@ -3,11 +3,7 @@
 import os
 import requests
 from datetime import datetime
-from dotenv import load_dotenv
 from flask import Flask, request, jsonify, Response, stream_with_context
-
-# load dotenv
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '../'))
 
 # init
 app = Flask(__name__)
@@ -81,36 +77,6 @@ def llm_chat():
         return jsonify({"error": f"Failed to connect to local LLM: {str(e)}"}), 502
     except Exception as e:
         return jsonify({"error": f"An error occurred: {str(e)}"}), 500
-
-
-# @app.route('/api/chat', methods=['POST'])
-# def llm_chat():
-#     # check for API key in the request
-#     data = request.get_json()
-#     if 'key' not in data or data['key'] != app.config['PROXY_KEY']:
-#         return jsonify({"error": "Invalid or missing API key"}), 401
-    
-#     # remove key before forwarding to LLM
-#     data.pop("key", None)
-    
-#     # check if client wants streaming
-#     stream = data.get("stream", True)
-    
-#     # generate result
-#     try:
-#         # remove the key from the data before forwarding to LLM
-#         data.pop('key', None)
-#         data['stream'] = False
-
-#         # forward the request to the local LLM
-#         response = requests.post('http://localhost:11434/api/chat', json=data)
-        
-#         # return the LLM's response
-#         return jsonify(response.json()), response.status_code 
-#     except requests.exceptions.RequestException as e:
-#         return jsonify({"error": f"Failed to connect to local LLM: {str(e)}"}), 502
-#     except Exception as e:
-#         return jsonify({"error": f"An error occurred: {str(e)}"}), 500
 
 
 if __name__ == '__main__':
